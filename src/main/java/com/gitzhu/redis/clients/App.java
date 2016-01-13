fba901b2b64350543ddb5648f6b7b28ec32681cc
qiuxu.zhu <qiuxu.zhu@qunar.com>
2016-01-13 15:46:31 +0800

    init maven project

---
+++ src/main/java/com/gitzhu/redis/clients/App.java
@@ -0,0 +1,13 @@
+package com.gitzhu.redis.clients;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
